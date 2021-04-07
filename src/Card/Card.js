653f7e908b120701a1ae025e48f5d5a328938825
Josh Aragon <josharagon99@gmail.com>
2021-04-07 09:56:13 -0600

    Add cancel button with styling

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -4,10 +4,11 @@ import './Card.css';
 const Card = ({name, date, time, numOfGuests}) => {
     return (
         <div className='card'>
-            <h2>Name: {name}</h2>
-            <h2>Date: {date}</h2>
-            <h2>Time: {time}</h2>
+            <h2>{name}</h2>
+            <h2>{date}</h2>
+            <h2>{time}</h2>
             <h2>Guests: {numOfGuests}</h2>
+            <button className='cancel-button'>Cancel</button>
         </div>
     )
 }
